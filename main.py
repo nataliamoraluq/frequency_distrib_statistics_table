7081f97c8827f09cbd94b07ccd34d972372ac2b9
nataliamoraluq <mora.luq.natalia68@gmail.com>
2024-10-14 11:27:14 -0400

    trying to push into git repos

---
+++ main.py
@@ -0,0 +1,30 @@
+#MAIN CLASS
+
+#DATOS TABULADOS (SIN ORDENAR)#
+
+#CALCULOS
+"""
+--------------------------------
+(MTC - MEDIDAS DE TENDENCIA CENTRAL)
+1.	media aritmética, 
+2.	moda,
+3.	 mediana,
+4.	 varianza,
+5.	 desviación típica.
+6.	P65
+7.	Q3
+8.	D3
+9.	D8
+10.	Q1
+-----------------------------------------
+"""
+
+"""
+-----------------------------------------
+(MDV - MEDIDAS DE VARIABILIDAD)
+11.	CV
+12.	Rango intercuartil
+13.	Índice de asimetría
+14.	Curtosis
+-----------------------------------------
+"""
---
+++ testClass.py
@@ -0,0 +1,15 @@
+#vista data frama con panda
+import pandas as pd
+# Columnas: features
+data_0 = {'sexo': ['Masculino','Femenino','Otro'], 'codigo': [0,1,2], 'frecuencia': [5,6,2]}
+pd_0 = pd.DataFrame(data=data_0)
+pd_0
+
+#vista dataframe con matplotlib
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+dias = ["L","M","M","J","V","S","D"]
+temperaturas = {'BsAs': [28.5,30.5,31,30,28,27.5,30.5],'SantiagoDelEstero': [35,36,38,49,47,42,31]}
+ax.plot(dias,temperaturas['BsAs'],color='tab:purple', label='Buenos Aires', marker="^")
+ax.plot(dias,temperaturas['SantiagoDelEstero'],color='tab:green', marker='*',label='SantiagoDelEstero')

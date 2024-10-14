3b00a82b04f09317a55b17f8ab342ad49c40a70b
nataliamoraluq <mora.luq.natalia68@gmail.com>
2024-10-14 11:56:54 -0400

    github

--- testClass.py
+++ testClass.py
@@ -13,3 +13,6 @@ dias = ["L","M","M","J","V","S","D"]
 temperaturas = {'BsAs': [28.5,30.5,31,30,28,27.5,30.5],'SantiagoDelEstero': [35,36,38,49,47,42,31]}
 ax.plot(dias,temperaturas['BsAs'],color='tab:purple', label='Buenos Aires', marker="^")
 ax.plot(dias,temperaturas['SantiagoDelEstero'],color='tab:green', marker='*',label='SantiagoDelEstero')
+
+#pip install scipy
+#pip install statsmodels -> works with panda i think

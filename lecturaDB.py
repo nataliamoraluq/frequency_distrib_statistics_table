a290ef5d60307ca36cb666d3391d67fbf296cbfa
nataliamoraluq <mora.luq.natalia68@gmail.com>
2024-10-30 16:51:34 -0400

    probando barras

--- lecturaDB.py
+++ lecturaDB.py
@@ -1,1 +1,31 @@
 #prueba lectura de datos db
+import csv
+import matplotlib.pyplot as plt
+#
+valores = []
+edad = []
+salarioW = []
+horasW = []
+#
+def readDB():
+    with open("DBdatosProyecto2024.csv", newline='') as csvFile:
+        spamreader = csv.reader(csvFile, delimiter=' ', quotechar=' ')
+        i = 0
+        for row in spamreader:
+            if i > 0:
+                data = row[0].split(',')
+                valores.append(data[1])
+                edad.append(data[2])
+                salarioW.append(data[4])
+                horasW.append(data[5])
+            i += 1
+#
+"""
+plt.hist(valores)
+print(valores)
+plt.show()
+"""
+readDB()
+plt.hist(edad)
+print(edad)
+plt.show()
